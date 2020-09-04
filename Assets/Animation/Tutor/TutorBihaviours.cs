eba887dffc8a54f77c603a27c80b97168ba6c8f3
Qubik <ElshinAleksandr@mail.ru>
2020-09-04 06:41:27 +0300

    Tutor_2

---
+++ Assets/Animation/Tutor/TutorBihaviours.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorBihaviours : MonoBehaviour
+{
+    [SerializeField] GameObject firstText;
+ 
+    Animator anim;
+    int step;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void NextStep() 
+    {
+        if (step == 0)
+            firstText.SetActive(false);
+
+        step++;
+        anim.SetTrigger($"Step_{step}");
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
